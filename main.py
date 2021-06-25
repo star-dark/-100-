0489a597dde234b86cd294dc3e7e421bd4411b9f
stardark2 <74770897+star-dark@users.noreply.github.com>
2021-06-25 14:02:28 +0000

    Day1 Step3 imputer를 simpleimputer로 바꿔주었다.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.impute import SimpleImputer
 dataset = pd.read_csv('Data.csv') #Dataset 지정
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , :3].values
+imputer = SimpleImputer(missing_values = np.nan, strategy = "mean")
+imputer = imputer.fit(X[ : , 1:3])
+X[ : , 1:3] = imputer.transform(X[ : , 1:3])
