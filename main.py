f94496139756fd9a853fbf34689e4f10f882955b
stardark2 <74770897+star-dark@users.noreply.github.com>
2021-06-25 14:48:43 +0000

    Day1 Step6 데이터 스케일링

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
 #Dataset 지정
 dataset = pd.read_csv('Data.csv') 
 X = dataset.iloc[ : , :-1].values
@@ -21,3 +22,7 @@ onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 # DATA SET을 훈련용 DATA와 시험용 DATA로 나누기
 X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
+# 데이터 스케일링 (표준 스케일링)
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
