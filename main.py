eff4a4252491ca1493bc9d055d21e6f1d0995624
stardark2 <74770897+star-dark@users.noreply.github.com>
2021-06-25 14:42:30 +0000

    Day1 Step5 Dataset 나누기 8:2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.cross_validation import train_test_split
 #Dataset 지정
 dataset = pd.read_csv('Data.csv') 
 X = dataset.iloc[ : , :-1].values
@@ -18,3 +19,5 @@ Y =  labelencoder_Y.fit_transform(Y)
 # 더미변수 만들기
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
+# DATA SET을 훈련용 DATA와 시험용 DATA로 나누기
+X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
